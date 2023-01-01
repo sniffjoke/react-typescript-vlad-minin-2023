062a02390f688e51b473dfb2df985e639d786b22
sniffjoke <41764936+sniffjoke@users.noreply.github.com>
2023-01-01 23:25:09 +0100

    full project

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <>
+            <p className={'text-center'}>Loading...</p>
+        </>
+    );
+};
+
+export default Loader;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing.
+        </div>
+    );
+};
+
+export default AboutPage;
